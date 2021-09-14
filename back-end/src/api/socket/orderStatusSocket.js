cfcc6d48934c4f40ed303b641c8c33035b68b610
Pedro Henrique <pedro@lifelink.com.br>
2021-09-14 12:20:42 -0300

    finish socket

---
+++ back-end/src/api/socket/orderStatusSocket.js
@@ -0,0 +1,15 @@
+const { changeStatusDelivery } = require('../service/orderServices');
+
+const orderStatus = (io) => {
+  io.on('connection', (socket) => {
+    console.log('nova conexão' + socket.id);
+
+    socket.on('changeStatusOrder', async (status, id) => {
+      console.log(status, id);
+      io.emit('changeStatusOrder', status, id);
+      await changeStatusDelivery(id, status);
+    });
+  });
+};
+
+module.exports = orderStatus;
