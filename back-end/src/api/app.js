ee270e9e3e2d03d791920af9e4db219641d31eeb
MarceloBorges <melombm@hotmail.com>
2021-09-13 23:18:20 -0500

    inicio de socket

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser').json();
 const path = require('path');
+const http = require('http');
 const cors = require('cors');
 const router = require('./router/router');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 app.use(bodyParser);
+const httpServer = http.createServer(app);
 
-const options = {
-  origin: 'http://localhost:3000',
-};
+// const options = {
+//   origin: 'http://localhost:3000',
+// };
+app.use(cors());
 
-app.use(cors(options));
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }});
+
+  io.on('connection', (socket) => {
+    console.log(`Usuário conectado. ID: ${socket.id} `);
+  });
+  
 
 app.use('/images', express.static(path.join(__dirname, '..', '..', 'public')));
 app.get('/coffee', (_req, res) => res.status(418).end());
@@ -21,4 +33,5 @@ app.use(router);
 
 app.use(errorHandler);
 
-module.exports = app;
+// module.exports = app;
+module.exports = httpServer;
---
+++ front-end/src/socket.js
@@ -0,0 +1,5 @@
+import { oi } from 'socket.io-client';
+
+const socket = io('http://localhost:3001');
+
+export default socket;
