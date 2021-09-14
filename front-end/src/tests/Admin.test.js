460c78e8c4bdec4f575a8874faf9757ab6aeb2c0
cleber-teixeira <cleberlt@gmail.com>
2021-09-14 13:36:38 -0300

    Adiciona testes ao arquivo Admin.test.js

--- front-end/src/tests/Admin.test.js
+++ front-end/src/tests/Admin.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
+import { fireEvent } from '@testing-library/react';
 
 describe('Tela de Admin', () => {
   const token = 'eyJhbGciOiJIUzI1NiJ9.YWRtQGRlbGl2ZXJ5YXBwLmNvbQ.2gYh-bSOjBInsijAAWWNUcJ9K8_869Q93AmgdEn3NY8';
@@ -17,9 +18,93 @@ describe('Tela de Admin', () => {
     history.push('/admin/manage');
   });
 
+  test('Verifica se renderiza o texto "Delivery App Admin"', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const text = getByText(/Delivery App Admin/i);
+
+    expect(text).toBeInTheDocument();
+  });
+
   test('Verifica se há o botão "Sair" na tela Admin', () => {
     const { getByTestId } = renderWithRouter(<App />);
-    const linkProductsTestId = getByTestId(/customer_products__element-navbar-link-logout/);
-    expect(linkProductsTestId).toBeInTheDocument();
+    const testId = getByTestId(/customer_products__element-navbar-link-logout/);
+    expect(testId).toBeInTheDocument();
+  });
+
+  test('Verifica se renderiza o texto "Nome"', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const text = getByText(/Nome/i);
+
+    expect(text).toBeInTheDocument();
+  });
+
+  test('Verifica se há o campo "Nome"', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+
+    const testId = getByTestId(/admin_manage__input-name/);
+
+    expect(testId).toBeInTheDocument();
+  });
+
+  test('Verifica se há o campo "Email"', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+
+    const testId = getByTestId(/admin_manage__input-email/);
+
+    expect(testId).toBeInTheDocument();
+  });
+
+  test('Verifica se há o campo "Senha"', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+
+    const testId = getByTestId(/admin_manage__input-password/);
+
+    expect(testId).toBeInTheDocument();
+  });
+
+  test('Verifica se há o campo "Tipo"', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+
+    const testId = getByTestId(/admin_manage__select-role/);
+
+    expect(testId).toBeInTheDocument();
+  });
+
+  test('Verifica se há o botão "Cadastrar"', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+
+    const testId = getByTestId(/admin_manage__button-register/);
+
+    expect(testId).toBeInTheDocument();
+  });
+
+  test('Alterando o valor do campo "Nome" e testando o valor guardado', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+
+    const input = getByTestId(/admin_manage__input-name/);
+    expect(input).toHaveValue('');
+
+    fireEvent.change(input, { target: { value: 'Zezinho do Silva' } });
+    expect(input).toHaveValue('Zezinho do Silva');
+  });
+
+  test('Alterando o valor do campo "Email" e testando o valor guardado', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+
+    const input = getByTestId(/admin_manage__input-email/);
+    expect(input).toHaveValue('');
+
+    fireEvent.change(input, { target: { value: 'zezinho@email.com' } });
+    expect(input).toHaveValue('zezinho@email.com');
+  });
+
+  test('Alterando o valor do campo "Senha" e testando o valor guardado', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+
+    const input = getByTestId(/admin_manage__input-password/);
+    expect(input).toHaveValue('');
+
+    fireEvent.change(input, { target: { value: 'zezinho123' } });
+    expect(input).toHaveValue('zezinho123');
   });
 });
